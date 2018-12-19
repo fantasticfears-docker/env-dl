87684c1f33f4582c6a569445778673f12baff925
Erick Guan <fantasticfears@gmail.com>
2018-12-19 10:24:14 +0100

    add push command

---
+++ push_container.py
@@ -0,0 +1,22 @@
+
+import sys
+import os.path
+from subprocess import call
+from build_container import gen_tag, push_container, print_command
+
+def push_container(dir):
+    filename = os.path.join(dir, "Dockerfile")
+    metadata = read_metadata(filename)
+
+    tag_name = gen_tag(metadata['name'], metadata['version'])
+    minor_tag = gen_tag(metadata['name'], metadata['minor_version'])
+    cmd = ["docker", "tag", tag_name, minor_tag]
+    print_command(cmd)
+    call(cmd)
+
+    latest_tag = gen_tag(metadata[name], 'latest')
+    cmd = ["docker", "tag", tag_name, latest_tag]
+    call(cmd)
+
+if __name__ == '__main__':
+    push_container(sys.argv[1])
